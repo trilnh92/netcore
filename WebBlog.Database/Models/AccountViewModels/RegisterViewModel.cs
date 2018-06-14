4d4b651c74963ea37fbde01183103f94082d7f17
erickkkt <tony-hoang@outlook.com>
2018-06-13 22:24:11 -0700

    Update model view

--- WebBlog.Database/Models/AccountViewModels/RegisterViewModel.cs
+++ WebBlog.Database/Models/AccountViewModels/RegisterViewModel.cs
@@ -9,6 +9,21 @@ namespace WebBlog.Database.Models.AccountViewModels
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2}, at max {1} characters long and unique.", MinimumLength = 2)]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
