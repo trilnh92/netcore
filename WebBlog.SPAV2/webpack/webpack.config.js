589ba34b157e12c7e6ee3bfdbfe713eacd5dc1d4
trilnh92@gmail.com <trilnh92@gmail.com>
2018-07-26 00:20:50 +0700

    ~ refactor webpack

--- WebBlog.SPAV2/webpack/webpack.config.js
+++ WebBlog.SPAV2/webpack/webpack.config.js
@@ -4,11 +4,16 @@ const htmlWebpackPlugin = new HtmlWebpackPlugin({
     template: path.join(__dirname, "index.html"),
     filename: "./index.html"
 });
+
 module.exports = {
-    entry: path.join(__dirname, "src/index.jsx"),
+    entry: {
+       index: path.join(__dirname, "src/index.jsx")
+    },
     output: {
         path: path.join(__dirname, "dist"),
-        filename: "bundle.js"
+        filename: "bundle.js",
+        chunkFilename:'[id][hash].js',
+        publicPath:'/'
     },
     module: {
         rules: [
